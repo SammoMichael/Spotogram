f099c6b3c594b17ab845f30a37c4a14dc8624c7b
Samuel Michael <samadammichael@yahoo.com>
2019-01-31 22:12:48 -0500

    first layout

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default{
+    images: {
+        heartIcon: require("../../assets/images/heart.png"),
+        arrowIcon: require("../../assets/images/arrow.png"),
+        bubbleIcon: require("../../assets/images/bubble.png"),
+    },
+    styleConstants: {
+        rowHeight: 50
+    }
+ };
